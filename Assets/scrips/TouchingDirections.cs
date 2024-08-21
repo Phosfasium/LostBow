2f34a10fb578ec18cc2ea104fccfb3b1aec09114
Phosfasium <wsloof2998@gmail.com>
2024-08-21 14:27:45 +0200

    Tileset Setup

---
+++ Assets/scrips/TouchingDirections.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchingDirections : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    CapsuleCollider2D touchingCol;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
